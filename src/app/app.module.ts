c85c761cdc6aff67998f2f04e9383edbb3ae094f
HaidukAA <108396767+HaidukAA@users.noreply.github.com>
2022-07-15 17:36:08 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
